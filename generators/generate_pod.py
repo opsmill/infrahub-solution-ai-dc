4438339dd612994db1ceaec1097134e6e265e963
Damien Garros <dgarros@gmail.com>
2025-09-07 15:31:59 +0200

    Add rack generator

--- generators/generate_pod.py
+++ generators/generate_pod.py
@@ -9,13 +9,15 @@ from infrahub_sdk.protocols import CoreIPAddressPool, CoreIPPrefixPool
 
 from solution_ai_dc.addressing import assign_ip_addresses_to_p2p_connections
 from solution_ai_dc.cabling import build_cabling_plan, connect_interface_maps
+from solution_ai_dc.generator import GeneratorMixin
+from solution_ai_dc.protocols import LocationRack, LocationRackBuilder, NetworkDevice, NetworkInterface, NetworkPod
 from solution_ai_dc.sorting import create_sorted_device_interface_map
 
 if TYPE_CHECKING:
     from infrahub_sdk.node import InfrahubNode
 
 
-class PodGenerator(InfrahubGenerator):
+class PodGenerator(InfrahubGenerator, GeneratorMixin):
     pod_id: str
     pod_index: int
     pod_name: str
@@ -26,29 +28,34 @@ class PodGenerator(InfrahubGenerator):
     loopback_pool: CoreIPAddressPool
 
     pod_prefix_pool: CoreIPPrefixPool
+    spine_switches: list[NetworkDevice]
 
     logger = logging.getLogger("infrahub.tasks")
 
     async def generate(self, data: dict) -> None:
-        self.pod_id: str = data["NetworkPodBuilder"]["edges"][0]["node"]["building_block"]["node"]["id"]
-        self.pod_index: int = data["NetworkPodBuilder"]["edges"][0]["node"]["building_block"]["node"]["index"]["value"]
-        self.pod_name: str = data["NetworkPodBuilder"]["edges"][0]["node"]["building_block"]["node"]["name"][
-            "value"
-        ].lower()
-        self.fabric_id: str = data["NetworkPodBuilder"]["edges"][0]["node"]["building_block"]["node"]["parent"]["node"][
-            "id"
-        ]
-        self.fabric_name: str = data["NetworkPodBuilder"]["edges"][0]["node"]["building_block"]["node"]["parent"][
-            "node"
-        ]["name"]["value"].lower()
+        self.pod_id: str = data["NetworkPodBuilder"]["edges"][0]["node"]["target"]["node"]["id"]
+        self.pod_index: int = data["NetworkPodBuilder"]["edges"][0]["node"]["target"]["node"]["index"]["value"]
+        self.pod_name: str = data["NetworkPodBuilder"]["edges"][0]["node"]["target"]["node"]["name"]["value"].lower()
+        self.fabric_id: str = data["NetworkPodBuilder"]["edges"][0]["node"]["target"]["node"]["parent"]["node"]["id"]
+        self.fabric_name: str = data["NetworkPodBuilder"]["edges"][0]["node"]["target"]["node"]["parent"]["node"][
+            "name"
+        ]["value"].lower()
+        self.spine_switches = []
 
         await self.allocate_resource_pools()
 
-        # Create the spine switches
-        spine_switches = []
+        await self.create_spine_switches()
+
+        await self.connect_spine_to_super_spine()
+
+        await self.update_checksum()
+
+    async def create_spine_switches(self) -> None:
+        """Create the spine switches"""
+
         for idx in range(1, 5):
             device = await self.client.create(
-                "NetworkDevice",
+                NetworkDevice,
                 hostname=f"spine-{self.pod_name}-{idx}",
                 object_template={"hfid": ["Spine Switch"]},
                 pod={"id": self.pod_id},
@@ -56,41 +63,7 @@ class PodGenerator(InfrahubGenerator):
                 role="spine",
             )
             await device.save(allow_upsert=True)
-            spine_switches.append(device)
-
-        # Connect the Spine to the SuperSpine
-        fabric_pod = await self.client.get(kind="NetworkPod", parent__ids=[self.fabric_id], role__value="fabric")
-        super_spine_switches = await self.client.filters(
-            kind="NetworkDevice", pod__ids=[fabric_pod.id], role__value="super_spine"
-        )
-
-        spine_interfaces = await self.client.filters(
-            kind="NetworkInterface", device__ids=[spine.id for spine in spine_switches], role__value="super_spine"
-        )
-        spine_interface_map = create_sorted_device_interface_map(spine_interfaces)
-
-        super_spine_interfaces = await self.client.filters(
-            kind="NetworkInterface", device__ids=[ss.id for ss in super_spine_switches], role__value="spine"
-        )
-        super_spine_interface_map = create_sorted_device_interface_map(super_spine_interfaces)
-
-        created_cabling_plan: list[tuple[InfrahubNode, InfrahubNode]] = build_cabling_plan(
-            logger=self.logger,
-            pod_index=self.pod_index,
-            src_interface_map=spine_interface_map,
-            dst_interface_map=super_spine_interface_map,
-        )
-
-        await connect_interface_maps(client=self.client, logger=self.logger, cabling_plan=created_cabling_plan)
-
-        await assign_ip_addresses_to_p2p_connections(
-            client=self.client,
-            logger=self.logger,
-            connections=created_cabling_plan,
-            prefix_len=31,
-            prefix_role="pod_super_spine_spine",
-            pool=self.pod_prefix_pool,
-        )
+            self.spine_switches.append(device)
 
     async def allocate_resource_pools(self) -> None:
         """Allocate IP Space for the Pod"""
@@ -132,3 +105,54 @@ class PodGenerator(InfrahubGenerator):
             resources=[pod_loopback_prefix],
         )
         await self.loopback_pool.save(allow_upsert=True)
+
+        pod = await self.client.get(kind=NetworkPod, id=self.pod_id)
+        pod.loopback_pool = self.loopback_pool
+        pod.prefix_pool = self.pod_prefix_pool
+        await pod.save(allow_upsert=True)
+
+    async def connect_spine_to_super_spine(self) -> None:
+        fabric_pod = await self.client.get(kind=NetworkPod, parent__ids=[self.fabric_id], role__value="fabric")
+        super_spine_switches = await self.client.filters(
+            kind=NetworkDevice, pod__ids=[fabric_pod.id], role__value="super_spine"
+        )
+
+        spine_interfaces = await self.client.filters(
+            kind=NetworkInterface, device__ids=[spine.id for spine in self.spine_switches], role__value="super_spine"
+        )
+        spine_interface_map = create_sorted_device_interface_map(spine_interfaces)
+
+        super_spine_interfaces = await self.client.filters(
+            kind=NetworkInterface, device__ids=[ss.id for ss in super_spine_switches], role__value="spine"
+        )
+        super_spine_interface_map = create_sorted_device_interface_map(super_spine_interfaces)
+
+        created_cabling_plan: list[tuple[InfrahubNode, InfrahubNode]] = build_cabling_plan(
+            logger=self.logger,
+            pod_index=self.pod_index,
+            src_interface_map=spine_interface_map,
+            dst_interface_map=super_spine_interface_map,
+        )
+
+        await connect_interface_maps(client=self.client, logger=self.logger, cabling_plan=created_cabling_plan)
+
+        await assign_ip_addresses_to_p2p_connections(
+            client=self.client,
+            logger=self.logger,
+            connections=created_cabling_plan,
+            prefix_len=31,
+            prefix_role="pod_super_spine_spine",
+            pool=self.pod_prefix_pool,
+        )
+
+    async def update_checksum(self) -> None:
+        racks = await self.client.filters(kind=LocationRack, pod__ids=[self.pod_id])
+        rack_trackers = await self.client.filters(kind=LocationRackBuilder, target__ids=[rack.id for rack in racks])
+
+        # store the checksum for the fabric in the object itself
+        checksum = self.calculate_checksum()
+        for rack_tracker in rack_trackers:
+            if rack_tracker.checksum.value != checksum:
+                rack_tracker.checksum.value = checksum
+                await rack_tracker.save(allow_upsert=True)
+                self.logger.info(f"Rack Tracker {rack_tracker.name.value} has been updated to checksum {checksum}")
